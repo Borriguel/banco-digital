f3e41f57f33daeead39a714034cea106cde3c3a1
Borriguel <rodolpho-henrique@hotmail.com.br>
2023-06-26 03:25:53 -0300

    alterada anotação de @ControllerAdvice para @RestControllerAdvice e inserção da anotação @ResponseStatus

--- src/main/java/dev/borriguel/bancodigital/exception/RestExceptionHandler.java
+++ src/main/java/dev/borriguel/bancodigital/exception/RestExceptionHandler.java
@@ -1,49 +1,49 @@
 package dev.borriguel.bancodigital.exception;
 
-import java.time.Instant;
-
-import javax.servlet.http.HttpServletRequest;
-
 import dev.borriguel.bancodigital.exception.custom.ErroTransacaoException;
 import dev.borriguel.bancodigital.exception.custom.RecursoDuplicadoException;
 import dev.borriguel.bancodigital.exception.custom.RecursoInvalidoException;
 import dev.borriguel.bancodigital.exception.custom.SaldoInsuficienteException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@RestControllerAdvice
 public class RestExceptionHandler {
     @ExceptionHandler(RecursoInvalidoException.class)
-    public ResponseEntity<DetalhesException> recursoInvalido(RecursoInvalidoException e, HttpServletRequest http) {
-        DetalhesException err = DetalhesException.builder()
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public DetalhesException recursoInvalido(RecursoInvalidoException e, HttpServletRequest http) {
+        return DetalhesException.builder()
                 .status(HttpStatus.NOT_FOUND.value())
                 .erro("Entidade não encontrada")
                 .diretorio(http.getRequestURI())
                 .mensagem(e.getMessage())
                 .timestamp(Instant.now())
                 .build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 
     @ExceptionHandler(RecursoDuplicadoException.class)
-    public ResponseEntity<DetalhesException> recursoDuplicado(RecursoDuplicadoException e, HttpServletRequest http) {
-        DetalhesException err = DetalhesException.builder()
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public DetalhesException recursoDuplicado(RecursoDuplicadoException e, HttpServletRequest http) {
+        return DetalhesException.builder()
                 .status(HttpStatus.BAD_REQUEST.value())
                 .erro("Dados duplicados")
                 .diretorio(http.getRequestURI())
                 .mensagem(e.getMessage())
                 .timestamp(Instant.now())
                 .build();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidacaoException> validation(MethodArgumentNotValidException e, HttpServletRequest http) {
-        ValidacaoException err = ValidacaoException.builder()
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidacaoException validation(MethodArgumentNotValidException e, HttpServletRequest http) {
+        ValidacaoException exception = ValidacaoException.builder()
                 .timestamp(Instant.now())
                 .erro("Erro de validação")
                 .mensagem("Verifique o campo erros para mais detalhes")
@@ -51,32 +51,32 @@ public class RestExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST.value())
                 .build();
         for (FieldError f : e.getBindingResult().getFieldErrors()) {
-            err.adicionarErro(f.getField(), f.getDefaultMessage());
+            exception.adicionarErro(f.getField(), f.getDefaultMessage());
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+        return exception;
     }
 
     @ExceptionHandler(SaldoInsuficienteException.class)
-    public ResponseEntity<DetalhesException> saldoInsuficiente(SaldoInsuficienteException e, HttpServletRequest http) {
-        DetalhesException err = DetalhesException.builder()
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public DetalhesException saldoInsuficiente(SaldoInsuficienteException e, HttpServletRequest http) {
+        return DetalhesException.builder()
                 .status(HttpStatus.BAD_REQUEST.value())
                 .erro("Saldo insuficiente")
                 .diretorio(http.getRequestURI())
                 .mensagem(e.getMessage())
                 .timestamp(Instant.now())
                 .build();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
     @ExceptionHandler(ErroTransacaoException.class)
-    public ResponseEntity<DetalhesException> erroTransacao(ErroTransacaoException e, HttpServletRequest http) {
-        DetalhesException err = DetalhesException.builder()
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public DetalhesException erroTransacao(ErroTransacaoException e, HttpServletRequest http) {
+        return DetalhesException.builder()
                 .status(HttpStatus.BAD_REQUEST.value())
                 .erro("Erro ao efetuar transação")
                 .diretorio(http.getRequestURI())
                 .mensagem(e.getMessage())
                 .timestamp(Instant.now())
                 .build();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
