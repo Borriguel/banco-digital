d872b66967d8241af44396c1eb63eb93d596fff8
Borriguel <rodolpho-henrique@hotmail.com.br>
2023-06-27 02:00:22 -0300

    adicionado informações do projeto na documentação

--- src/main/java/dev/borriguel/bancodigital/BancoDigitalApplication.java
+++ src/main/java/dev/borriguel/bancodigital/BancoDigitalApplication.java
@@ -1,9 +1,23 @@
 package dev.borriguel.bancodigital;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Banco Digital Rest API",
+				description = "Documentação da API Rest Spring Boot",
+				contact = @Contact(
+						name = "Rodolpho Henrique",
+						email = "rodolpho.omedio@gmail.com",
+						url = "https://github.com/Borriguel"
+				)
+		)
+)
 public class BancoDigitalApplication {
 
 	public static void main(String[] args) {
