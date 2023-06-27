9ec40b212207b1700564850eda69edc5cafd870d
Borriguel <rodolpho-henrique@hotmail.com.br>
2023-06-27 01:34:43 -0300

    renomeado de LojistaDTO para LojistaResponse

--- src/main/java/dev/borriguel/bancodigital/controller/dto/LojistaResponse.java
+++ src/main/java/dev/borriguel/bancodigital/controller/dto/LojistaResponse.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 
 @Data
 @NoArgsConstructor
-public class LojistaDTO {
+public class LojistaResponse {
     private UUID id;
     private String cnpj;
     private String nomeEmpresa;
